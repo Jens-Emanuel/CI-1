0f562ab7ca5c7cb977a2841b9f350d8d81c964db
Jens-Emanuel <122988642+Jens-Emanuel@users.noreply.github.com>
2023-02-08 15:31:21 +0100

    Add files via upload
    Testar vad som händer när jag lägger till en fil i min rep.

---
+++ testinet.cy.js
@@ -0,0 +1,26 @@
+
+describe('First tests', () => {
+  
+  it('Enter ikea', () => {
+    //Visit website
+    cy.visit('https://www.ikea.se')
+    cy.wait(500)
+    //accept all cookies
+    cy.get('#onetrust-accept-btn-handler').click()
+    //search for item
+    cy.get('.search-field__input').type('billy {ENTER}')
+    //add to basket
+    cy.get('.plp-product-list__products > :nth-child(1) > .plp-product-list__fragment > :nth-child(1) > .pip-product-compact > .pip-product-compact__bottom-wrapper > .pip-product-compact__buttons-container > .pip-btn--icon-emphasised').click()
+    cy.wait(1000)
+    
+    //get basket
+    cy.scrollTo('top')
+    cy.wait(1000)
+    cy.get('.hnf-header__shopping-cart-link > .hnf-btn > .hnf-btn__inner').click()
+      .should('have.length',1)
+    cy.get('.cart-ingka-price__integer').should('contain', 599)
+
+
+    
+  })
+})
