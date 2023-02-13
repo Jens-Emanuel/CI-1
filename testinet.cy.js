7119148e66b41623828f71f7f63f725e2998d151
Jens-Emanuel <122988642+Jens-Emanuel@users.noreply.github.com>
2023-02-13 23:33:43 +0100

    Ändrar namnet på mitt test

--- testinet.cy.js
+++ testinet.cy.js
@@ -1,7 +1,8 @@
-
 describe('First tests', () => {
+
+
   
-  it('Enter ikea', () => {
+  it('Asserting adding to basket and that item "billy" comes in on the right price', () => {
     //Visit website
     cy.visit('https://www.ikea.se')
     cy.wait(500)
@@ -12,12 +13,12 @@ describe('First tests', () => {
     //add to basket
     cy.get('.plp-product-list__products > :nth-child(1) > .plp-product-list__fragment > :nth-child(1) > .pip-product-compact > .pip-product-compact__bottom-wrapper > .pip-product-compact__buttons-container > .pip-btn--icon-emphasised').click()
     cy.wait(1000)
-    
+
     //get basket
     cy.scrollTo('top')
     cy.wait(1000)
     cy.get('.hnf-header__shopping-cart-link > .hnf-btn > .hnf-btn__inner').click()
-      .should('have.length',1)
+      .should('have.length',1);
     cy.get('.cart-ingka-price__integer').should('contain', 599)
 
 
